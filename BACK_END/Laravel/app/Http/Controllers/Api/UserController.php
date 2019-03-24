24c2098a15003e8a1e513448dbf959b7f01068cf
TRANVIETHUY <huytran161297@gmail.com>
2019-03-24 12:30:12 +0700

    update api

--- BACK_END/Laravel/app/Http/Controllers/Api/UserController.php
+++ BACK_END/Laravel/app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use DB;
 
 class UserController extends Controller
 {
