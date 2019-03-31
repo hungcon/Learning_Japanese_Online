14cb1ae94ea0b333e08842ae90156edd75f7ed0c
HungPv <hungcon.5070@gmail.com>
2019-03-31 22:09:32 +0700

    update choose

--- FRONT_END/src/Components/ContentChoose.js
+++ FRONT_END/src/Components/ContentChoose.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 
 class ContentChoose extends Component {
     render() {
-        const question = this.props.question;
-        console.log(question);
-        
-        const box = '<span class="q-3-dot">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
-        
-        $(function () {
-            $('.q-abcd-answer').click(function (event) {
-                var arr = $(".q-abcd-answer");
-                console.log(arr.length);
-                arr.each(function (index, el) {
-                    $(el).removeClass('actived');
-                });
-                $(this).addClass('actived')
-            });
-            $('.q-question-choose').append(question.replace(/-/g, box));
-        });
         return (
             <p className="q-question q-question-choose"></p>
         );
+    }
+
+    componentDidMount = () => {
+        const rawQuestion = this.props.question;    
+        var box = '<span class="q-3-dot">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+       var abc = document.getElementsByClassName('q-question-choose');
+       var question = rawQuestion.replace(/-/g, box);
+       abc[0].innerHTML = question;
     }
 }
 
