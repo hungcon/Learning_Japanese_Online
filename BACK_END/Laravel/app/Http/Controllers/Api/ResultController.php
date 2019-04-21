f237cf2c3159909694c20e1de74c8fcdd1f91bc9
hoan <nhatvan023@gmail.com>
2019-04-21 16:02:57 +0700

    fix result page

--- BACK_END/Laravel/app/Http/Controllers/Api/ResultController.php
+++ BACK_END/Laravel/app/Http/Controllers/Api/ResultController.php
@@ -8,36 +8,29 @@ use DB;
 class ResultController extends Controller
 {
     public function getHighScoreTest(Request $request){
-    	try{
-
+        try{
             $idUser = $request->id;
-            $hightScore = DB::table('history')->max('mark');
-            // Lấy id của lesson có điểm cao nhất và bài làm gần nhất 
-            $test = DB::table('history')->where('id_user',$idUser)->where('mark',$hightScore)->orderBy('date','DESC')->first();
-       
-            $results = DB::table('history')->where('id_user',$idUser)->join('lesson','lesson.id','history.id_lesson')->select('history.id','history.id_lesson','history.mark','history.date','lesson.name')->get();
 
-            $historyHighScore = array();
-            $nameRule = \App\Lesson::find($test->id_lesson)->JoinToRule->name;
+            $results = DB::table('result')->where('id_user',$idUser)->join('lesson','lesson.id','result.id_lesson')->get();
+            
+            $dataResults= array();
 
             foreach ($results as $result) {
-            	if( $result->id === $test->id){
-            		$historyHighScore= array(
-		            	'mark'=>$result->mark,
-		                'date'=>$result->date,
-		                'id_lesson'=>$result->id_lesson,
-		                'nameRule'=>$nameRule,
-		                'nameLesson'=>$result->name
-		            );
-            	}
+                $id_lesson = $result->id_lesson;
+                $nameRule = \App\Lesson::find($id_lesson)->JoinToRule->name;
+                $dataResults[] = array(
+                    'mark'=>$result->mark,
+                    'id_lesson'=>$result->id_lesson,
+                    'nameRule'=>$nameRule,
+                    'nameLesson'=>$result->name
+                );
             }
-          
+
             if($results === null){
                 return response()->json(["message" => "User dont test ","historyHighScore" => "Chưa thực hiện bài test "], 200);
             }else{
-                return response()->json(["message" => "User tested ","historyHighScore" => $historyHighScore], 200);
+                return response()->json(["message" => "User tested ","historyHighScore" =>$dataResults], 200);
             }
-
         }catch (\Exception $exception){
             return response()->json(["error" => $exception->getMessage()],200);
         }
--- FRONT_END/src/Components/Topbar.js
+++ FRONT_END/src/Components/Topbar.js
@@ -173,21 +173,21 @@ class Topbar extends React.Component {
                                 <img className="img-profile rounded-circle" src="/img/ava1.jpg" alt="img" />
                             </a>
     
-                            <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a className="dropdown-item" href="/">
+                            <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" >
+                                <a className="dropdown-item" href="/" style={{ fontSize: 16 }}>
                                     <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400" />
                                     Profile
                                 </a>
-                                <a className="dropdown-item" href="/">
+                                <a className="dropdown-item" href="/" style={{ fontSize: 16 }}>
                                     <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400" />
                                     Settings
                                 </a>
-                                <a className="dropdown-item" href="/">
+                                <a className="dropdown-item" href="/" style={{ fontSize: 16 }}>
                                     <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400" />
                                     Activity Log
                                 </a>
                                 <div className="dropdown-divider" />
-                                <a className="dropdown-item" href="/" onClick={this.handleLogout} data-toggle="modal" data-target="#logoutModal">
+                                <a className="dropdown-item" href="/" onClick={this.handleLogout} data-toggle="modal" data-target="#logoutModal" style={{ fontSize: 16 }}>
                                     <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
                                     Logout
                                 </a>
