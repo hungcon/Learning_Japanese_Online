1616d3d38289115a8bca0ddbaab288490376089d
Chungbien <chungch251997@gmail.com>
2019-04-21 22:58:46 +0700

    fixx css

--- FRONT_END/src/Components/Topbar.js
+++ FRONT_END/src/Components/Topbar.js
@@ -174,20 +174,20 @@ class Topbar extends React.Component {
                             </a>
     
                             <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a className="dropdown-item" href="/">
+                                <a className="dropdown-item user--item" href="/">
                                     <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400" />
                                     Profile
                                 </a>
-                                <a className="dropdown-item" href="/">
+                                <a className="dropdown-item user--item" href="/">
                                     <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400" />
                                     Settings
                                 </a>
-                                <a className="dropdown-item" href="/">
+                                <a className="dropdown-item user--item" href="/">
                                     <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400" />
                                     Activity Log
                                 </a>
-                                <div className="dropdown-divider" />
-                                <a className="dropdown-item" href="/" onClick={this.handleLogout} data-toggle="modal" data-target="#logoutModal">
+                                <div className="dropdown-divider user--item" />
+                                <a className="dropdown-item user--item" href="/" onClick={this.handleLogout} data-toggle="modal" data-target="#logoutModal">
                                     <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
                                     Logout
                                 </a>
